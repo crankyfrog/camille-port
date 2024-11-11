affb0f2deb61866d9316fff7e102247e8ec72ef4
Camille Tindogan <camilletindogan@Camilles-MacBook-Pro.local>
2024-11-11 17:27:36 +0100

    update menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,27 +1,41 @@
+import { useLocation } from 'react-router-dom';
 import './Menu.css';
 
-function Menu(){
+function Menu() {
+    const location = useLocation();
     const links = [
         {
             title: 'Home',
-            link: '/Home'
+            link: '/Home',
+            id: 1
         },
         {
             title: 'Resume',
-            link: '/Resume'
+            link: '/Resume',
+            id: 2
         },
         {
             title: 'LinkedIn',
-            link: 'https://www.linkedin.com/in/camilleux/'
+            link: 'https://www.linkedin.com/in/camilleux/',
         }
-    ]
-    const navLinks =
-    links.map((link,index) =>{
-        return(
-            <a className="menuItem" key={index} href={link.link}>{link.title}</a>
-        )
-    })
-    return(
+    ];
+
+    const navLinks = links.map((link, index) => {
+        // Check if the current link matches the current location
+        const isActive = location.pathname === link.link;
+
+        return (
+            <a
+                className={`menuItem small-caps ${isActive ? 'active-link' : ''}`}
+                key={index}
+                href={link.link}
+            >
+                {link.title}
+            </a>
+        );
+    });
+
+    return (
         <nav className='Menu col col-3'>{navLinks}</nav>
     );
 }
